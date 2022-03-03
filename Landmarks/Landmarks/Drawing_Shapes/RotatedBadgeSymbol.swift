f4e49a5e862302fa7a21356743f368be116ff003
Lautner-kwangho <wooree2716@khcu.ac.kr>
2022-03-03 16:51:51 +0900

    Totorial complete

---
+++ Landmarks/Landmarks/Drawing_Shapes/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by 최광호 on 2022/03/02.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
+
